6ead5468a7b8cbd19ef7eafbe9bd8c6192092657
bskim <kbs03@hanyang.ac.kr>
2024-05-20 10:34:21 +0900

    피드백 반영

--- src/get_lmz_path.py
+++ src/get_lmz_path.py
@@ -1,12 +1,17 @@
 import os
 
 
-def get_lmz_path():
-    dat_path = "..\\dat\\"
+def get_lmz_path(wafer, device):
+    if __name__ == "__main__":
+        dat_path = "..\\dat\\"
+    else:
+        dat_path = "dat\\"
 
     batch_path = dat_path + os.listdir(dat_path)[0] + "\\"
 
-    wafer_paths = [batch_path + wafer_path + "\\" for wafer_path in os.listdir(batch_path)]
+    if not wafer:
+        wafer = os.listdir(batch_path)
+    wafer_paths = [batch_path + wafer_path + "\\" for wafer_path in wafer]
 
     measure_date_paths = []
     for wafer_path in wafer_paths:
@@ -17,12 +22,15 @@ def get_lmz_path():
     lmz_path = []
     for measure_date_path in measure_date_paths:
         for file_name in os.listdir(measure_date_path):
-            if "LMZ" in file_name:
-                lmz_path.append(measure_date_path + file_name)
-    print("Found LMZ data in dat folder: ")
+            if any(d in file_name for d in device):
+                if __name__ == "__main__":
+                    lmz_path.append(measure_date_path + file_name)
+                else:
+                    lmz_path.append('..\\' + measure_date_path + file_name)
+    print(f"Found {len(lmz_path)} LMZ data in dat folder: ")
     print(lmz_path)
     return lmz_path
 
 
 if __name__ == "__main__":
-    lmz_path = get_lmz_path()
+    get_lmz_path([], ['LMZC', 'LMZO'])
