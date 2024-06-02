47fbda59e98dc69bcb434ab7da9c988dabaf0894
Eunji Son <eunjeeok2@naver.com>
2024-06-02 18:00:15 +0900

    240530

--- src/operate.py
+++ src/operate.py
@@ -9,7 +9,7 @@ def run(info, wafer, device, save_figure, export_csv):
     lmz_paths = get_lmz_path(wafer, device)
     for lmz_path in lmz_paths:
         vi_result = vi(lmz_path)
-        trans_result = 0
+        trans_result = trans_process(lmz_path) # 원래 trans_result = 0
         if save_figure:
             plot_figure(vi_result, trans_result, lmz_path)
         if export_csv:
