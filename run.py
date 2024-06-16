c8fda1cb62a7179e409983bb29bccc0789d905e6
Beomseo Kim <bskim030427@naver.com>
2024-06-16 22:59:34 +0900

    v1.0.0 change

--- run.py
+++ run.py
@@ -4,12 +4,12 @@ from src.operate import run
 info = {
     'script_id': "Process LMZ",
     'script_owner': 'Team 1',
-    'script_version': 0.1,
+    'script_version': '1.0.0',
     'operator_name': "",
 }
 wafer = []  # ['D07', 'D08', ...]. Blank out list to process all wafers
 device = ['LMZC', 'LMZO']
-save_figure = False
+save_figure = True
 export_excel = True
 # ---------------------------- #
 
